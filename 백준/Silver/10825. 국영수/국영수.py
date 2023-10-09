496d391d1506efe308b7c73516c005bf4c6ad268
greenTea31 <38368218+greenTea31@users.noreply.github.com>
2023-10-10 00:21:59 +0900

    [Silver IV] Title: 국영수, Time: 600 ms, Memory: 160480 KB -BaekjoonHub

---
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -0,0 +1,10 @@
+N = int(input())
+students = []
+
+for _ in range(N):
+    students.append(list(input().split()))
+
+students.sort(key=lambda x: (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
+
+for student in students:
+    print(student[0])
